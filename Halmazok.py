fd5753861cd677eec5f0abdc6e06ea986e406d64
Tamás Győri <gyorit@GDO.LAN>
2021-11-22 13:35:53 +0100

    2021 november 22

---
+++ Halmazok.py
@@ -0,0 +1,13 @@
+diak = {
+      'vezeteknev': 'Kiss',
+      'keresztnev': 'Péter',
+      'osztaly': '10.A',
+      'eletkor': 17,
+      'cim': {
+          'iranyitoszam': 9400,
+          'varos': 'Sopron',
+          'kozterulet': 'Fő utca',
+          'hazszam': 5
+        }
+    }
+print(diak['cim']['iranyitoszam'])
